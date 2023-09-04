42e99325d8a20e67c03453b24b63fa32a9f9e297
Simon Janlöv <simon.janlov@gmail.com>
2023-09-04 13:39:10 +0200

    get_db_connection() and read_phonelist

--- dbapp.py
+++ dbapp.py
@@ -1,9 +1,31 @@
 from flask import Flask, render_template, request
 import datetime
+import psycopg2
+
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432", # Usually port number 5432 for PostgreSQL
+        database="phonedb",
+        user="postgres",
+        password="pac112") # Change to your own pgAdmin postgres user password
+    return conn
 
 simple = [
   ['arne', '013-131313'], ['berith','01234'], ['caesar','077-1212321']
 ]
+
+def read_phonelist():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM phonelist;")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+
 
 app = Flask(__name__)
 
@@ -16,4 +38,14 @@ def start():
     if len(D[2])<2:
         D[2] = '0'+D[2]
     return render_template('list.html', list=simple, date=D)
+
+@app.route("/insert", methods = ['POST', 'GET'])
+def insert_page():
+    if request.method =='POST':
+        name=request.form['name']
+        phone=request.form['phone']
+        return render_template('insert.html', req=insert_contact(name,
+            phone, address, city, email))
+    else: # GET method
+        return render_template('list.html', list=read_phonelist())
 
